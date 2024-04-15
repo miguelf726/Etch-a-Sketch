972d9d803946a5aef0735332eb45835d9ef66e21
Miguel Flores <miguel.flores72623@gmail.com>
2024-04-15 02:03:22 -0400

    Add a hover effect that turns the cells black

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 const container = document.querySelector("#container");
-const body = document.querySelector("body")
-const button = document.createElement("button")
-
+const body = document.querySelector("body");
+const button = document.createElement("button");
+const cell = document.createElement("div");
+cell.setAttribute("class", "cell");
 
 function createGrid() {
   for (let r = 0; r < 16; r++) {
     for (let c = 0; c < 16; c++) {
-      const cell = document.createElement("div");
-
+      let cell = document.createElement("div");
       container.appendChild(cell);
+      cell.addEventListener('mouseover',()=> {
+       cell.style.backgroundColor="black"
+    })
     }
+    
   }
+ 
 }
 
-createGrid();
+createGrid()
+
+
