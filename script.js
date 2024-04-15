f515f84850a7b1f19c8ad9077c6281f4738e6573
Miguel Flores <miguel.flores72623@gmail.com>
2024-04-15 02:16:07 -0400

    Remove the creation of a button in the js file

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
 const container = document.querySelector("#container");
 const body = document.querySelector("body");
-const button = document.createElement("button");
-const cell = document.createElement("div");
-cell.setAttribute("class", "cell");
+
 
 function createGrid() {
   for (let r = 0; r < 16; r++) {
     for (let c = 0; c < 16; c++) {
       let cell = document.createElement("div");
       container.appendChild(cell);
-      cell.addEventListener('mouseover',()=> {
-       cell.style.backgroundColor="black"
-    })
+      cell.addEventListener("mouseover", () => {
+        cell.style.backgroundColor = "black";
+      });
     }
-    
   }
- 
 }
 
-createGrid()
-
-
+createGrid();
